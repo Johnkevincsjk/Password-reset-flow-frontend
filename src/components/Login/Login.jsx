1b62f396994350a98f51aba147e95b5c468e5173
johnkevincsjk <johnkevin.csjk@gmail.com>
2024-06-18 00:34:16 +0530

    formik and yup updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,45 +5,72 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { toast } from 'react-toastify';
 import { motion } from 'framer-motion'
+import { useFormik } from "formik"
 
 
 export default function Login() {
     const Nav = useNavigate()
-    const [Mailid, setMailid] = useState('')
-    const [Password, setPassword] = useState('')
+    // const [Mailid, setMailid] = useState('')
+    // const [Password, setPassword] = useState('')
 
-    const handleLogin = async (e) => {
-        e.preventDefault()
-        const login_crd = { Mailid, Password }
+    // const handleLogin = async (e) => {
+    //     e.preventDefault()
+    //     const login_crd = { Mailid, Password }
 
 
-        try {
-            const resp = await axios.post('http://localhost:8000/resetflow/loginUser', login_crd)
+    //     try {
+    //         const resp = await axios.post('http://localhost:8000/resetflow/loginUser', login_crd)
 
-            if (resp.data.Error) {
-                toast.error(resp.data.Feedback)
-            } else {
-                Nav('/LandingPage')
-                toast.success(resp.data.Feedback)
+    //         if (resp.data.Error) {
+    //             toast.error(resp.data.Feedback)
+    //         } else {
+    //             Nav('/LandingPage')
+    //             toast.success(resp.data.Feedback)
+    //         }
+    //     } catch (error) {
+    //         toast.error(error.response?.data?.Feedback)
+    //         console.log(error)
+    //     }
+    // }
+
+    const { handleBlur, handleChange, values, handleSubmit, errors } = useFormik({
+        initialValues: {
+            Mailid: "",
+            Password: ""
+        },
+
+        onSubmit: async (values) => {
+            try {
+                const resp = await axios.post('http://localhost:8000/resetflow/loginUser', values)
+                if (resp.data.Error) {
+                    toast.error(resp.data.Feedback)
+                } else {
+                    Nav('/LandingPage')
+                    toast.success(resp.data.Feedback)
+                }
+            } catch (error) {
+                toast.error(error.response?.data?.Feedback)
+                console.log(error)
             }
-        } catch (error) {
-            toast.error(error.response?.data?.Feedback)
-            console.log(error)
         }
-    }
+
+    })
 
     return (
         <div className='Login_main'>
             <div className="Login_head">
                 <h5>Login</h5>
             </div>
-            <form onSubmit={handleLogin}>
+            <form onSubmit={handleSubmit}>
                 <div className="Login_one">
                     <p>Username</p>
                     <div className="input-group mb-3">
                         <input
-                            value={Mailid}
-                            onChange={(e) => setMailid(e.target.value)}
+                            name='Mailid'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.Mailid}
+
                             type="email" placeholder='enter mail id' className="form-control" aria-label="Text input with checkbox" />
                     </div>
 
@@ -52,8 +79,10 @@ export default function Login() {
                     <p>Password</p>
                     <div className="input-group mb-3">
                         <input
-                            value={Password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            name='Password'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.Password}
                             type="password" placeholder='enter password' className="form-control" aria-label="Text input with checkbox" />
                     </div>
                 </div>
